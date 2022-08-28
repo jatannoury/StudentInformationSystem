0de5035d79cd514907e97ad9ecc15aeed9256b8a
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-28 21:35:21 +0300

    Django | add: Add addToAvailableCourses route

--- sis-backend/admin/urls.py
+++ sis-backend/admin/urls.py
@@ -1,11 +1,12 @@
+from urllib import request
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path('',views.getRoutes),
     path("sayHi/",views.sayHi),
     path("signUp/",views.signUp),
     path("logIn/",views.logIn),
     path("addCourse/",views.addCourse),
+    path("addToAvailableCourses/",views.addToAvailableCourses)
     
 ]

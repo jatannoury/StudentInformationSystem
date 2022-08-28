be9418e11db01a87bbac27456a265b2a60ce3dc9
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-28 16:25:51 +0300

    Django | mod: Modify database

--- sis-backend/Model/models.py
+++ sis-backend/Model/models.py
@@ -11,3 +11,28 @@ class sisUser(models.Model):
     isAdmin=models.BooleanField(default=False)
     created=models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.fname
+
+class courses(models.Model):
+    code=models.CharField(max_length=7)
+    nb_of_credits=models.IntegerField()
+    title=models.CharField(max_length=200)
+    faculty=models.CharField(max_length=10)
+    department=models.CharField(max_length=10)
+    
+    def __str__(self):
+        return self.title
+    
+class availableCourses(models.Model):
+    semester=models.CharField(max_length=12)
+    code=models.ForeignKey(courses,on_delete=models.CASCADE)
+    instructor=models.CharField(max_length=200)
+    registered_seats=models.IntegerField()
+    available_seats=models.IntegerField()
+    time=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.fname
+    
+    

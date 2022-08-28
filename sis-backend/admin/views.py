e42b5070ff12558e4e6e0744ee7c8015b975bd42
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-28 14:26:49 +0300

    Django | mod: check for existing email | SignUp endpoint

--- sis-backend/admin/views.py
+++ sis-backend/admin/views.py
@@ -43,6 +43,28 @@ def signUp(request):
         fname[i]=fname[i].lower()[0]
         email+=fname[i]
     email+="@university.com"
+    # check for registered student having similar auto generated email
+    while(sisUser.objects.filter(email=email).values()):
+        print(email)
+        similar_user_email=sisUser.objects.filter(email=email).values()
+        if len(similar_user_email)!=0:
+            email=email.split("@")
+            registered_user_email=similar_user_email[0]['email'].split("@")
+            registered_email_user_name=registered_user_email[0]
+            try:
+                int(registered_email_user_name[len(registered_email_user_name)-2:])
+                new_user_code=int(registered_email_user_name[len(registered_email_user_name)-2:])
+                if new_user_code<10:
+                    email[0]=email[0][:len(email[0])-2]+"0"+str(new_user_code+1)
+                    email=email[0]+email[1]
+                else:
+                    email[0]=email[0][:len(email[0])-2]+str(new_user_code)
+                    email=email[0]+email[1]
+                print(email)
+            except:
+                email[0]+="01"
+                email=email[0]+"@"+email[1]
     newUser=sisUser(id=new_user_id,fname=data['fname'],email=email,password=make_password(data['password']))
     newUser.save()
+    
     return Response({'message':'succesful'})

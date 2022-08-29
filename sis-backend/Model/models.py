6fd19bb8d7ec369b80c955687fda135adacd373b
Joseph Tannoury <jatannouy01@gmail.com>
2022-08-30 00:21:15 +0300

    Django | add: Add registerClass endpoint

--- sis-backend/Model/models.py
+++ sis-backend/Model/models.py
@@ -1,19 +1,21 @@
 from email.policy import default
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 from django.db import models
 
 class sisUser(models.Model):
     id=models.CharField(primary_key=True,max_length=8)
+    user_type=models.IntegerField(default=0)
     fname=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     password=models.CharField(max_length=100)
-    isAdmin=models.BooleanField(default=False)
     admin_allows_registration=models.BooleanField(default=False)
     created=models.DateTimeField(auto_now_add=True)
     is_registered_in_curr_sem=models.BooleanField(default=False)
     student_status=models.CharField(default="Not Registered",max_length=20)
+    is_instructor=models.BooleanField(default=False)
     
     def __str__(self):
         return self.fname
@@ -32,11 +34,23 @@ class availableCourses(models.Model):
     semester=models.CharField(max_length=12)
     code=models.CharField(max_length=12)
     instructor=models.CharField(max_length=200,default="Staff")
+    room=models.CharField(max_length=20,default="TBA")
     registered_seats=models.IntegerField()
     available_seats=models.IntegerField()
     time=models.CharField(max_length=200)
     
     def __str__(self):
         return self.code
+
+
+class registeredCourses(models.Model):
+    student_id=models.CharField(max_length=12)
+    registered_class_id=models.CharField(max_length=10)
+    instructor_name=models.CharField(max_length=10)
+    time=models.CharField(max_length=200)
+    room=models.CharField(max_length=20)
+    grade=models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(4.0)],default="0.00")
     
+    def __str__(self):
+        return self.student_id
     
--- sis-backend/user/views.py
+++ sis-backend/user/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import make_password,check_password
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from Model.models import sisUser,availableCourses,courses
+from Model.models import sisUser,availableCourses,courses,registeredCourses
 from Model.serializers import UserSerializer
 from Model import serializers
 import datetime
@@ -10,13 +10,15 @@ import json
 # Create your views here.
 
 #Helper Functions
-
+def getAdmin():
+    return sisUser.objects.filter(isAdmin=True).values()
 
 @api_view(['GET'])
 def getRoutes(request):
     routes=[
         'GET /api',
         'GET /api/searchCourse',
+        'GET /api/registerClass',
         
     ]
     return Response(routes)
@@ -36,3 +38,13 @@ def searchCourse(request):
     else:
         return Response(all_courses.all().values())    
         
+@api_view(["POST"])
+def registerClass(request):
+    admin=getAdmin()
+    if admin[0]['admin_allows_registration']==False:
+        return Response({"message":"Not Registration period"})
+    data=json.loads(request.body)
+    new_course_registered=registeredCourses(student_id=data['student_id'],registered_class_id=data['registered_class_id'],instructor_name=data['instructor_name'],time=data['time'],room=data['room'])
+    new_course_registered.save()
+    return Response({"message":"Succesful"})
+    
